cedecdf55f7a48d115be5474892d42203bf8c676
Joris Stork <joris@wintermute.eu>
2012-05-22 17:16:12 +0200

    cleaned up the code and paths. still debugging tho.

--- assignments/3_pca/src/5.3-eigenstructure.py
+++ assignments/3_pca/src/5.3-eigenstructure.py
@@ -1,12 +1,8 @@
 #!/usr/bin/env python
 """
-:synopsis:  Uses the incremental method for calculating the covariance derived
-            in exercise 5.1, to obtain the covariance matrix for the set of
-            25x25 details in a 256x256 image. Then plots the scree diagram of
-            eigenvalues of the covariance matrix, and plots the eigenvectors
-            as images. Finally, reduces the dimensionality of a detail with
-            respect to the principal component basis, and uses the result to
-            reconstruct a similar detail in the original coordinate system.
+:synopsis:  Analysis of local structure of an image (lab exercise 5.3). Uses
+            the incremental method for calculating the covariance derived in
+            exercise 5.1
             
 
 .. moduleauthor:: Joris Stork <joris@wintermute.eu>, Lucas Swartsenburg
@@ -21,65 +17,71 @@ import numpy as np
 import os
 
 
+def eigsorted(X):
+    """ Returns the eigenvectors and eigenvalues sorted in descending order """
+
+    l, V = np.linalg.eig(X)
+    si = np.argsort(l)[-1::-1]
+    return l[si], V[:,si]
+
+
+def plotter(nr, data):
+    """ Auxiliary function: all required plots, grouped for convenience  """
+
+    if nr is 1:
+        x = data
+        plt.figure()
+        plt.subplot(1,2,1)
+        plt.imshow(x, cmap=plt.cm.gray)
+        d = x[75:100,170:195]
+        plt.subplot(1,2,2)
+        plt.imshow(d, cmap=plt.cm.gray)
+        plt.savefig('data/image_with_detail.pdf')
+    if nr is 2:
+        l = data
+        plt.figure()
+        plt.bar(xrange(len(l)), l)
+        plt.savefig('data/5.3_scree.pdf')
+    if nr is 3:
+        V = data
+        plt.figure()
+        for j in xrange(6):
+            plt.subplot(2,3,j)
+            plt.imshow(V[j].reshape(25,25), cmap=plt.cm.gray)
+        plt.savefig('data/5.3_eigenvectors.pdf')
+
+
 def main():
     """
-    1. reads image
-    2. takes detail
-    3. saves figure with image beside detail
-    4. finds covariance of elements of the sample that consists of all 25x25px
-        details of the image.
-    5. saves scree diagram of eigenvalues of covariance matrix from (4)
-    6. saves plots of first 6 eigenvectors
-    7. displays detail and its reconstruction from k eigenvectors
+    1. reads image, saves figure: image beside 25x25 detail
+    2. finds cov(dataset of all 25x25px details) 
+    3. plots scree diagram of eigenvalues of cov. matrix
+    4. plots first 6 eigenvectors
+    5. saves figure: detail and reconstruction from k eigenvectors
 
     """
 
-    x = plt.imread('yard.jpg')[::-1,:] # image flipped ...
+    x = plt.imread('data/image.jpg')[-1::-1,:]          # image (de-flipped)
     h, w = x.shape
     xsize = h*w
 
-    plt.figure()
-    plt.subplot(1,2,1)
-    plt.imshow(x, cmap=plt.cm.gray)
-    d = x[75:100,170:195]
-    plt.subplot(1,2,2)
-    plt.imshow(d, cmap=plt.cm.gray)
-    plt.savefig('yard_with_detail.pdf')
+    plotter(1, x)
     
-    xystep = 25
-    xisize = xystep * xystep # 
-    sxi = np.zeros((xisize,), dtype=float) # sum of measurements
-    sxixit = np.zeros((xisize,xisize),dtype=float) # holds sum(xi * xi.T)
-    S = None
-    n = 0.0
-    if os.path.isfile("5.3_cov.npy"):
-        S = np.load("5.3_cov.npy")
-    else:
+    # xi are the details in the image
+    xystep = 25                                         # width of one detail
+    xisize = xystep * xystep                            # size of one detail
+    sxi = np.zeros((xisize,), dtype=float)              # sum of details
+    sxixit = np.zeros((xisize,xisize),dtype=float)      # sum(xi * xi.T)
+    S = None                                            # covariance matrix
+    n = 0.0                                             # nr of details
+
+    if os.path.isfile("data/5.3_cov.npy"):              # try to recycle old S
+        S = np.load("data/5.3_cov.npy")
+    else:                                               # or else calculate S
         for j in xrange(h - xystep+1):
             for k in xrange(w - xystep+1):
-                n += 1
-                xi = x[j:j+xystep, k:k+xystep]
-                if j == 75 and k == 170:
-                    plt.figure()
-                    plt.subplot(1,1,1)
-                    plt.imshow(xi, cmap=plt.cm.gray)
-                    plt.savefig('samedetail.pdf')
-                if j*k == 10000:
-                    plt.figure()
-                    plt.subplot(1,1,1)
-                    plt.imshow(xi, cmap=plt.cm.gray)
-                    plt.savefig('randetail.pdf')
-                if j*k == 20000:
-                    plt.figure()
-                    plt.subplot(1,1,1)
-                    plt.imshow(xi, cmap=plt.cm.gray)
-                    plt.savefig('randetail2.pdf')
-                if j*k == 30000:
-                    plt.figure()
-                    plt.subplot(1,1,1)
-                    plt.imshow(xi, cmap=plt.cm.gray)
-                    plt.savefig('randetail3.pdf')
-                xi = xi.reshape(xisize)
+                n += 1.0
+                xi = x[j:j+xystep, k:k+xystep].reshape(xisize)
                 sxi += xi
                 sxixit += np.outer(xi, xi)
 
@@ -95,20 +97,12 @@ def main():
         print '\nm: \n', m
 
         S = (sxixit - (n * np.outer(m,m))) / (n - 1.0)
-        np.save('5.3_cov.npy', S)
+        np.save('data/5.3_cov.npy', S)
     print '\nS: \n', S
-    l, V = np.linalg.eig(S)
-    si = np.argsort(l)[::-1]
-    l = l[si]
-    V = V[:,si]
-    plt.figure()
-    plt.bar(xrange(len(l)), l)
-    plt.savefig('5.3_scree.pdf')
-    plt.figure()
-    for j in xrange(6):
-        plt.subplot(2,3,j)
-        plt.imshow(V[j].reshape(25,25), cmap=plt.cm.gray)
-    plt.savefig('5.3_eigenvectors.pdf')
+    l, V = eigsorted(S)
+
+    plotter(2, l)
+    plotter(3, V)
 
 
 if __name__ == "__main__":
