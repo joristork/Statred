24ba9c231ff3430a7e863ec9d9e7292f028da308
lswartsenburg <luuk@noregular.com>
2012-05-30 18:48:09 +0200

    done

--- assignments/4_svm/src/fortyseven.py
+++ assignments/4_svm/src/fortyseven.py
@@ -1,72 +1,110 @@
+#!/usr/bin/env python
 from pylab import *
+import sys
+import os
+from subprocess import *
 from svmutil import *
 
-class SVMClassifier:
-    def __init__(self, X, c, parameter):
-        self.model = svm_train(c.tolist(), X.tolist(), parameter)
+if len(sys.argv) <= 1:
+	print('Usage: {0} create_sets [testing_file]'.format(sys.argv[0]))
+	raise SystemExit
 
-    def classify(self, x):
-    
-        # svm_predict is a function from svmutil, it is intended to classify an
-        # entire test set a once. To classify just one sample we need the first
-        # element of labels.
-        labels, _, _ =  svm_predict([0], [x.tolist()], self.model)
-        return labels[0]
-        
-        
-# The colors
 colors = ['black', 'blue', 'brown', 'gray', 'green', 'orange', 'pink', 'red',
-          'violet', 'white', 'yellow']
+          'violet', 'white', 'yellow']  
+                
+def containsColor( line ):
+    for c in colors:
+        if line.find(c)>=0:
+            return colors.index(c), c
+    return None, None
         
-lines = open("natural400_700_5.asc").readlines()
+        
+#Start source from reader
+
+lines = open(sys.argv[1]).readlines()
+
+traindata_filename = 'trainset'
+testdata_filename = 'testset'
+
+trainset = open(traindata_filename, 'w')
+testset = open(testdata_filename, 'w')
+
 
 D = zeros((0, 61))
 y = array([])
-
-for i in range(0, len(lines), 2):
-   
-    ind = None
-    
-    for j, color in enumerate(colors):
-        if lines[i].find(color) >= 0:
-            ind = j
-     
+for i in range(0,len(lines),2):
+    ind, c = containsColor(lines[i])
     if ind is not None:
-        d = fromstring(lines[i+1], dtype=int, sep=" ")
-        D = append(D, array([d]), axis=0)
-        y = append(y, ind)
+        d = fromstring(lines[i+1],dtype=int,sep=" ")
+        D = append(D,array([d]),axis=0)
+        y= append(y,ind) 
         
-    
+#Calculate how large our dataset is
+count_data = len(D)
+#Create a random list of indices in the range of our data set
+random_i = permutation(arange(0, count_data))
+testindices = random_i[:count_data/3] # Test set
+trainindices = random_i[count_data/3:] # Learn set
 
-# Get random indices of the learn set and the test set. The test set is one 
-# third of the complete dataset and the learn set is two third.
-N = len(D)
-ind = permutation(arange(0, N))
+#Write the testdata to a libsvm readable file
+for i in testindices:
+    t = int(y[i])
+    line = "{}: ".format(t)
+    for j,val in enumerate(D[i]):
+        val_int = int(val)
+        line += "{}:{} ".format(j,val_int)
+    line += "\n"
+    testset.write(line)
+testset.close()
 
-T = ind[:N/3] # Test set
-L = ind[N/3:] # Learn set
+#Write the traindata to a libsvm readable file    
+for i in trainindices:
+    t = int(y[i])
+    line = "{}: ".format(t)
+    for j,val in enumerate(D[i]):
+        val_int = int(val)
+        line += "{}:{} ".format(j,val_int)
+    line += "\n"
+    trainset.write(line)    
+trainset.close()
+
+#Easy svm calculates the best kernel and kernelvalues to use on this trainingset. Easy then generates a svm model file which can be loaded to
+#make predictions later on. 
+cmd = '{0} {1} {2}'.format("python easy.py", traindata_filename, testdata_filename)
+print('Using easy.py to train the svm and scale both the train and the test set.')	
+f = Popen(cmd, shell = True, stdout = PIPE, stderr = STDOUT).stdout
+
+print "\n\n********** easy.py output **********"
+print f.read()
+print "\n"
+
+print "Loading " + traindata_filename + ".model " + "to start predicting values."
+svm = svm_load_model(traindata_filename + ".model")
+print "Loading the scaled testvalues from " + testdata_filename + ".scale."
+loaded_labels, scaled_testdata = svm_read_problem(testdata_filename + ".scale")
 
 
-# Construct the classifier, for more information about these parameters please
-# see the output of the easy script in 'easy_results/natural/natural.easy.model'.
-param = '-c 32768.0 -g 0.0001220703125'
+confusionmatrix = zeros((len(colors), len(colors)))
 
-classifier = SVMClassifier(D[L], y[L], param)
+#Calculate the confusion matrix
+percentage = zeros((2, len(colors)))
 
-
-# Create a confusion matrix
-CM = zeros((len(colors), len(colors)))
-
-for i in ind:
-    c = classifier.classify(D[i])
-    CM[int(y[i]), int(c)] += 1
+print "\n\n********** svm_predict function output **********"
+for i, data in enumerate(scaled_testdata):   
+    labels, _, _ =  svm_predict([0], [data.values()], svm)
+    c = labels[0]
+    confusionmatrix[int(loaded_labels[i]), int(c)] += 1
+    if int(loaded_labels[i]) == int(c):
+        percentage[0,loaded_labels[i]] += 1
+    else:
+        percentage[1, loaded_labels[i]] += 1
+print "\n\n"
 
 
 # Print the confusion matrix
-print 'Confusion matrix'
-
-for i in range(-1, len(colors)):
-    for j in range(-1, len(colors)):
+print '********** Confusion matrix ********** '
+for i in xrange(-1, len(colors)):
+    for j in xrange(-1, len(colors)):
     
         if i == -1 and j == -1:
             print '%10s' % '',
@@ -75,6 +113,33 @@ for i in range(-1, len(colors)):
         elif j == -1:
             print '%10s' % colors[i],
         else:
-            print '%10d' % CM[i, j],
+            print '%10d' % confusionmatrix[i, j],
 
     print
+    
+
+
+#Calculate percentages
+print"\n\n********** Percentages ********** "
+count = 0
+good = 0;
+for i in xrange(len(colors)):
+    total = percentage[0,i] + percentage[1,i]
+    count += total
+    if total != 0:
+        fraction = percentage[0,i] / float(total)
+        percent = fraction * 100.0
+        print colors[i] + "\t: " + str(percent) + "% correctly predicted"        
+    else:
+        print colors[i] + "\t: zero occurances, so technically: 100%"
+    good += percentage[0,i]
+
+
+print "Total\t: " + str(((good/count) * 100.0)) + "% correctly predicted"
+    
+    
+
+
+
+
+
